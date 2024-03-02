8ae62e34a2a84e610103d44f758bfcc245840832
Akila-jaya <akilajayawickrama7@gmail.com>
2024-03-02 19:25:32 +0530

    Add DTO class for token.

---
+++ springboot-backend/src/main/java/com/example/demo/dto/User.java
@@ -0,0 +1,42 @@
+package com.example.demo.dto;
+
+public class User {
+    String username;
+    String pwd;
+    String token;
+    String user;
+
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
