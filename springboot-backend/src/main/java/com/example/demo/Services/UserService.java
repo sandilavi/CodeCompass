0a6136cd3a78008bb6136fb7ed02e3cd90b43a4a
Akila-jaya <akilajayawickrama7@gmail.com>
2024-03-06 11:12:57 +0530

    add "check exists email" function.

--- springboot-backend/src/main/java/com/example/demo/Services/UserService.java
+++ springboot-backend/src/main/java/com/example/demo/Services/UserService.java
@@ -3,8 +3,10 @@ package com.example.demo.Services;
 import com.example.demo.Entity.User;
 import com.example.demo.dto.LoginDto;
 import com.example.demo.dto.UserDto;
+import com.example.demo.repository.ConfirmationTokenRepository;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
@@ -18,6 +20,8 @@ public class UserService {
     private UserRepository userRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    ConfirmationTokenRepository confirmationTokenRepository;
 
     UserDto userDto;
 
@@ -28,8 +32,12 @@ public class UserService {
                 userDto.getEmail(),
                 this.passwordEncoder.encode(userDto.getPassword())
         );
-        userRepository.save(user);
-        return user.getUserName();
+        if (userRepository.existsByUserEmail(user.getEmail())) {
+            return "Error: Email is already in use!";
+        }else{
+            userRepository.save(user);
+            return user.getUserName();
+        }
     }
 
     public LoginMesage loginUser(LoginDto loginDto) {
