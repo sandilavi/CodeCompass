b12aad1773f1441e684d0ab7a9c6a5274ec28614
Sandia Vihanga <122675509+sandilavi@users.noreply.github.com>
2024-03-08 12:06:02 +0530

    Create new component for the Quiz

--- reactjs-frontend/src/App.js
+++ reactjs-frontend/src/App.js
@@ -1,51 +1,20 @@
-import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from "react";
 import "./global.css";
-import SignInForm from "./components/login/Signin";
-import SignUpForm from "./components/login/Signup";
+import Home from "./components/login/Home";
+import Quiz from "./components/userEvaluate/Quiz";
 
-export default function App() {
-  const [type, setType] = useState("signIn");
-  const handleOnClick = text => {
-    if (text !== type) {
-      setType(text);
-      return;
-    }
-  };
-  const containerClass =
-    "container " + (type === "signUp" ? "right-panel-active" : "");
-
+function App() {
   return (
-    <div className="App">
-      <div className={containerClass} id="container">
-        <SignUpForm />
-        <SignInForm />
-        <div className="overlay-container">
-          <div className="overlay">
-            <div className="overlay-panel overlay-left">
-              <h1>Hi There!</h1>
-              <img className="logo" src={process.env.PUBLIC_URL + '/images/codecomp.png'} />
-              <button
-                className="ghost"
-                id="signIn"
-                onClick={() => handleOnClick("signIn")}
-              >
-                Sign In
-              </button>
-            </div>
-            <div className="overlay-panel overlay-right">
-              <h1>Welcome Back!</h1>
-              <img className="logo" src={process.env.PUBLIC_URL + '/images/codecomp.png'} />
-              <button
-                className="ghost "
-                id="signUp"
-                onClick={() => handleOnClick("signUp")}
-              >
-                Sign Up
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <BrowserRouter>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
+      </Routes>
+
+    </BrowserRouter>
   );
 }
+
+export default App;
---
+++ reactjs-frontend/src/components/userEvaluate/Quiz.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Quiz() {
+  return (
+    <div>
+      <h1>Hello Quiz!</h1>
+    </div>
+  )
+}
