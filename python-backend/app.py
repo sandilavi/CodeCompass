9b1c9894316627fd5bc48e14e3e52ae2a9ae1936
Sandia Vihanga <122675509+sandilavi@users.noreply.github.com>
2024-03-17 05:55:31 +0530

    Remove the render template of Flask

--- python-backend/app.py
+++ python-backend/app.py
@@ -1,12 +1,8 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 beginner_correct = 0
 intermediate_correct = 0
---
+++ python-backend/test_data.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import joblib
+
+# Load the trained model
+model = joblib.load('trained_model.joblib')
+
+# Enter the data which wants to test
+X_new_data = [
+    {'Total_Correct_Answers': 15, 'Beginner_Correct_Answers': 5, 'Intermediate_Correct_Answers': 5, 'Advanced_Correct_Answers': 5},
+]
+
+# Convert dictionary to DataFrame
+X_new_df = pd.DataFrame(X_new_data)
+
+# Select the relavant features
+selected_features = ['Total_Correct_Answers', 'Beginner_Correct_Answers', 'Intermediate_Correct_Answers', 'Advanced_Correct_Answers']
+X_new_selected = X_new_df[selected_features]
+
+# Convert DataFrame to numpy array
+X_new_array = X_new_selected.values
+
+# Use X_new_array for prediction
+predictions = model.predict(X_new_array)
+
+# Define a dictionary to map the predicted labels to user levels
+level_mapping = {1: 'Beginner', 3: 'Intermediate', 0: 'Advanced', 2: 'Expert'}
+
+# Convert the predictions to user levels using the mapping
+user_level = [level_mapping[prediction] for prediction in predictions]
+print("User Level:", user_level)
