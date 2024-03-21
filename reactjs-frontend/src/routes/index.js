202a69f7e7b161e69f1867d206dad58a47499cd7
TeshanLA <teshanlakruwan@gmail.com>
2024-03-22 01:03:25 +0530

    course content player

--- reactjs-frontend/src/routes/index.js
+++ reactjs-frontend/src/routes/index.js
@@ -3,12 +3,14 @@ import { useRoutes } from 'react-router-dom';
 // routes
 import MainRoutes from './MainRoutes';
 import { Login } from 'Login';
+import CourseContentPlayer from 'views/content-player/ContentPlayer';
 
 // ==============================|| ROUTING RENDER ||============================== //
 
 export default function ThemeRoutes() {
 
   const login = { path: '/login', element: <Login /> }
+  const player = { path: '/player', element: <CourseContentPlayer /> }
 
-  return useRoutes([...MainRoutes, login]);
+  return useRoutes([...MainRoutes, login, player]);
 }
