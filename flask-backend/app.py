c83c936a7f58f2d31b5653be831668432fa33538
sandilavi <sandilavihanga@gmail.com>
2024-07-13 11:38:19 +0530

    Update ports

--- flask-backend/app.py
+++ flask-backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd
 import joblib, json
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -52,8 +53,9 @@ def send_quiz_data():
     except Exception as e:
         return jsonify({'error': str(e)})
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
 #if __name__ == '__main__':
-#    app.run(host='0.0.0.0', port=8080)
+#    app.run(debug=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
